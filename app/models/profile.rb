adf3f3d50148da755e4a426b984b9aba252cab6c
Mateus Athaydes Martins <mateusathaydesmartins@Mateuss-MacBook-Air.local>
2016-10-13 16:09:06 -0300

    started relations ranking

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 require 'elasticsearch/model'
+require 'elasticsearch/dsl'
 require 'mongoid'
 require 'Date'
+require 'set'
 Mongoid.connect_to 'profiles'
 
 class Profile
@@ -13,11 +15,12 @@ class Profile
     # index name for keeping consistency among existing environments
     index_name "profiles-#{Rails.env}"
 
-    field :nome, type: String, default: ""
-    field :descricao, type: String, default: ""
+    field :nome, type: String, default: ''
+    field :descricao, type: String, default: ''
     field :producoes_bibliograficas, type: Array, default: []
     field :ranking_pessoal, type: Float, default: 0.0
-    
+    field :nome_citacoes, type: Array, default []
+
     embeds_many :orientacao, store_as: :orientados
     embeds_many :projeto_pesquisa, store_as: :projeto_pesquisa
     embeds_many :area_atuacao, store_as: :areas_atuacao
@@ -31,23 +34,69 @@ class Profile
     end
 
     set_callback(:save, :before) do |document|
+        document.nome_citacoes = document.create_nome_citacoes
+
         personal_ranking_value = document.calculate_personal_ranking
         document.ranking_pessoal = personal_ranking_value
     end
 
+    def create_nome_citacoes
+      # MÓRA, M. C.;MORA, M.C.;Móra, Michael da Costa;Mora, Michael da Costa
+      nome_citacoes = Array.new
+
+      # MICHAEL DA COSTA MORA
+      nome_citacoes.append(self.nome.upcase!)
+
+      nome_list = self.nome.split " "
+      last_name = nome_list.pop + ","
+
+      # MORA, MICHAEL DA COSTA
+      citacao = last_name + nome_list.join(' ')
+      nome_citacoes.append(citacao)
+
+      # MORA, M. C.
+      citacao = last_name
+      nome_list.each do |nome|
+        citacao += nome + "."
+      end
+      nome_citacoes.append(citacao)
+      return nome_citacoes
+    end
+
+    #
+    ## O ranking pessoal é dado por:
+    ## Média ponderada de:
+    ## (AR) Tempo em atividade recente - peso 3
+    ## (XP) Tempo de experiência - peso 1
+    ## (NA) Número de artigos - peso 4
+    ## (GR) Grau de formação - peso 2
+    #
     def calculate_personal_ranking
-        # O ranking pessoal é dado por:
-        # Média ponderada de:
-        # (AR) Tempo em atividade recente - peso 3
-        # (XP) Tempo de experiência - peso 1
-        # (NA) Número de artigos - peso 4
-        # (GR) Grau de formação - peso 2
         atividade_recente = self.get_atividade_recente
         experiencia = self.get_tempo_experiencia
         numero_artigos = self.get_numero_artigos
         grau_formacao = self.get_grau_formacao
 
         return (atividade_recente * 3 + experiencia * 1 + numero_artigos * 4 + grau_formacao * 2) / 10
+    end
+
+    #
+    ## O ranking de relacoes se trata do somatorio do ranking pessoal de cada um
+    ## dos perfis os quais o perfil atual se relaciona.
+    #
+    def calculate_relationship_ranking
+      # Descobrir as relacoes:
+        # Orientados
+        # Projetos de Pesquisa
+        # Producoes Bibliograficas
+        # Formacao academica?
+      # Consultar o RP de cada uma
+      # Somar os RPs e retornar
+      orientados = self.get_relations_of_orientados
+      projetos = self.get_relations_of_projetos_pesquisa
+      producoes = self.get_relations_of_producoes_bibliograficas
+
+      relations = orientados | projetos | producoes
     end
 
     protected
@@ -74,7 +123,7 @@ class Profile
         experience_years_count = 0
         last_experience_year = this_year
         self.formacao_academica.each do |formacao|
-            if formacao.data_fim != "Atual"
+            if formacao.data_fim != 'Atual'
                 if formacao.data_fim.to_i < last_experience_year.to_i
                     last_experiece_year = formacao.data_fim.to_i
                 end
@@ -95,24 +144,72 @@ class Profile
     protected
     def get_grau_formacao
         formacao = self.formacao_academica.sort_by { |formacao| formacao['data_fim'] }.reverse.first
-        if formacao.titulo.downcase.include? "doutor"
+        if formacao.titulo.downcase.include? 'doutor'
             return 4
-        elsif formacao.titulo.include? "mestr"
+        elsif formacao.titulo.include? 'mestr'
             return 3
-        elsif formacao.titulo.include? "especiali"
+        elsif formacao.titulo.include? 'especiali'
             return 2
-        elsif formacao.titulo.include? "gradua" and formacao.data_fim != "Atual"
+        elsif formacao.titulo.include? 'gradua' and formacao.data_fim != 'Atual'
             return 1
         else
             return 0
         end
     end
 
+    protected
+    def get_relations_of_orientados
+      orientados = Set.new
+      self.orientacao.each do |orientacao|
+        relation = Profile.find_by(:nome => orientacao.nome)
+        if relation
+          orientados.add(relation)
+        else
+          # Chama o servico python pra inserir a partir de orientacao.link
+          # Adiciona na lista de orientados
+        end
+      end
+      return orientados_relations
+    end
+
+    protected
+    def get_relations_of_projetos_pesquisa
+      relations = Set.new
+      self.projeto_pesquisa.each do |projeto|
+        integrantes = projeto.pesquisa[/Integrantes: .*\./].slice!('Integrantes: ')
+        integrantes.each do |integrante|
+          integrante_name = integrante.slice(/ - .*$/)
+          relation = Profile.find_by(:nome => integrante_name)
+          if relation
+            relations.add(relation)
+          else
+            # Chama o servico python pra inserir a partir de orientacao.link
+            # Adiciona na lista de orientados
+          end
+        end
+      end
+      return relations
+    end
+
+    protected
+    def get_relations_of_producoes_bibliograficas
+      relations = Set.new
+      sef.producoes_bibliograficas.each do |producao|
+        collaborators = proucao.split(/\ \. /).first.split(/ ; /)
+        collaborators.each do |collaborator|
+          collaborator.gsub!(/\s+/, "")
+          # ver se assinatura está em algum profile
+          Profile.find_by(:nome_citacoes => )
+          relations.add()
+        end
+      end
+      return relations
+    end
+
     def self.search_by(query)
-        __elasticsearch__.search({
-            sort: [ 
-                {'ranking_pessoal': {"order": "desc"}},
-                '_score', 
+      result = __elasticsearch__.search({
+            sort: [
+                {'ranking_pessoal': {"order": "desc"}}
             ],
             query: {
                 multi_match: {
@@ -126,9 +223,11 @@ class Profile
                         'projeto_pesquisa',
                         'orientados',
                         'premios',
-                        'idiomas'],
+                        'idiomas'
+                    ],
                 }
             }
         })
+        return result
     end
 end
