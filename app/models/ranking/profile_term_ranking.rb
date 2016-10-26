be213c0e0ed5f57d9aa61c5a2992131cc8de74a3
Mateus Athaydes Martins <mateusathaydesmartins@Mateuss-MacBook-Air.local>
2016-10-26 15:52:55 -0200

    added some structured methods and date_weight

--- app/models/ranking/profile_term_ranking.rb
+++ app/models/ranking/profile_term_ranking.rb
@@ -1,4 +1,22 @@
+require 'Date'
+
 class ProfileTermRanking
+
+  SECTION_WEIGHTS = {
+    'DP' => 3, # DESCRICAO DO PERFIL
+    'FR' => 3, # FORMACAO
+    'AA' => 3, # AREA DE ATUACAO
+    'OE' => 4, # ORGANIZACAO DE EVENTOS
+    'SP' => 13 # SECAO DE PUBLICACOES
+  }
+
+  # PESOS: SECAO DE PUBLICACOES
+  SP_SECTION_WEIGHTS = {
+    'PB' => 4, # PRODUCOES BIBLIOGRAFICAS
+    'OR' => 2, # ORIENTOU
+    'OP' => 1, # OUTRAS PRODUCOES
+    'PP' => 3  # PROJETOS DE PESQUISA
+  }
 
   def initialize(profile, terms)
     @profile = profile
@@ -35,11 +53,68 @@ class ProfileTermRanking
   end
 
   ##
+  # TODO este campo nao existe no profile ainda.
+  ##
+  def calculate_term_distance_for_events_section
+    total_events_distance = 0.0
+    return total_events_distance
+  end
+
+  ##
   # TODO
   # Publications_section has a separated method
   ##
   def calculate_term_distance_for_publications_section
-    #
+    mp_pb = self.calctulate_pub_term_for_publications_section
+    mp_or = self.calctulate_pub_term_for_oriented_section
+    mp_op = self.calculate_pub_term_for_other_productions_section
+    mp_pp = self.calculate_pub_term_for_research_projects_section
+
+    return (mp_pb * SECTION_WEIGHTS['PB'] + mp_or * SECTION_WEIGHTS['OR'] +
+      mp_op * SECTION_WEIGHTS['OP'] + mp_pp * SECTION_WEIGHTS['PP'])/10
+  end
+
+  ##
+  # TODO
+  ##
+  def calctulate_pub_term_for_publications_section
+  end
+
+  ##
+  # TODO
+  ##
+  def calctulate_pub_term_for_oriented_section
+  end
+
+  ##
+  # TODO
+  ##
+  def calculate_pub_term_for_other_productions_section
+  end
+
+  ##
+  # TODO
+  ##
+  def calculate_pub_term_for_research_projects_section
+  end
+
+  ##
+  # TODO
+  ##
+  protected
+  def get_weight_by_date(date_year)
+    this_year = Date.today.year
+    distance_from_today = this_year - date_year
+    case
+    when distance_from_today <= 3
+      return 10
+    when distance_from_today > 3 && distance_from_today <= 6
+      return 7
+    when distance_from_today > 6 && distance_from_today <= 10
+      return 5
+    when distance_from_today > 10
+      return 2
+    end
   end
 
   protected
@@ -93,11 +168,16 @@ class ProfileTermRanking
     return 0
   end
 
-  ##
-  # TODO
-  ##
-  def calculate_term_ranking(profile)
-    return 0.0
+  def calculate_term_ranking
+    dt_dp = self.calculate_term_distance_for_description_section
+    dt_fr = self.calculate_term_distance_for_graduation_section
+    dt_aa = self.calculate_term_distance_for_area_section
+    dt_oe = self.calculate_term_distance_for_events_section
+    dt_sp = self.calculate_term_distance_for_publications_section
+
+    return (dt_dp * SECTION_WEIGHTS['DP'] + dt_fr * SECTION_WEIGHTS['FR'] +
+      dt_aa * SECTION_WEIGHTS['AA'] + dt_oe * SECTION_WEIGHTS['OE']
+      dt_sp * SECTION_WEIGHTS['SP'])/26
   end
 
 end
--- app/models/ranking/search_result.rb
+++ app/models/ranking/search_result.rb
@@ -2,21 +2,6 @@ require_relative 'profile_term_ranking'
 
 class SearchResult
 
-  SECTION_WEIGHTS = {
-    'DP' => 3,
-    'FR' => 3,
-    'AA' => 3,
-    'OE' => 4,
-    'SP' => 13
-  }
-
-  SP_SECTION_WEIGHTS = {
-    'PB' => 4,
-    'OR' => 2,
-    'OP' => 1,
-    'PP' => 3
-  }
-
   def initialize(query, elasticsearch_result)
     @terms = query.split(' ')
     @elasticsearch_result = elasticsearch_result
@@ -24,8 +9,11 @@ class SearchResult
 
   def get_search_ordered_result
     @elasticsearch_result.each do |result|
-      profile_ranking = ProfileTermRanking.new(result, @terms)
-      profile_ranking
+      profile_term = ProfileTermRanking.new(result, @terms)
+      personal_ranking = result.ranking_pessoal
+      relations_ranking = result.ranking_relacoes
+      term_ranking = profile_term.calculate_term_ranking
+      # sort by: term, relations and then personal 
     end
   end
 
