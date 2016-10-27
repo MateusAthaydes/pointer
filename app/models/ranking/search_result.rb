fc3b6f03ff488e73aceae177f922fb7683e26862
Mateus Athaydes Martins <mateusathaydesmartins@Mateuss-MacBook-Air.local>
2016-10-27 16:07:14 -0200

    general_ranking and sort method created

--- app/models/ranking/search_result.rb
+++ app/models/ranking/search_result.rb
@@ -2,6 +2,12 @@ require_relative 'profile_term_ranking'
 
 class SearchResult
 
+  GENERAL_RANKING_WEIGHTS = {
+    'RP' => 3,
+    'RR' => 2,
+    'RT' => 5
+  }
+
   def initialize(query, elasticsearch_result)
     @terms = query.split(' ')
     @elasticsearch_result = elasticsearch_result
@@ -13,8 +19,14 @@ class SearchResult
       personal_ranking = result.ranking_pessoal
       relations_ranking = result.ranking_relacoes
       term_ranking = profile_term.calculate_term_ranking
-      # sort by: term, relations and then personal 
+      general_ranking = (personal_ranking * GENERAL_RANKING_WEIGHTS['RP'] + relations_ranking * GENERAL_RANKING_WEIGHTS['RR']
+        + term_ranking * GENERAL_RANKING_WEIGHTS['RT']) / 10
+
+      # ver se isso insere um atributo genreal_ranking no profile, ou ao menos no result.
+      result.general_ranking = general_ranking
     end
+    # Sort by general_ranking
+    @elasticsearch_result.sort! { |result1, result2| result1.general_ranking <=> result2 }
   end
 
   def say_hello
