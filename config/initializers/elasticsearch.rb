d041dc9a8afa51eeaa36384eb78ce75b1211898a
Mateus Athaydes Martins <mateusathaydesmartins@Mateuss-MacBook-Air.local>
2016-11-27 20:43:13 -0200

    env to mongo and bonsai

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] || "http://localhost:9200/"
+Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL'] || "http://localhost:9200/"
 
 unless Profile.__elasticsearch__.index_exists?
   Profile.__elasticsearch__.create_index! force: true
